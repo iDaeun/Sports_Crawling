9ab19b93cb379615360522fad6408cf3955100d9
daeun <programmerdaeun@gmail.com>
2019-12-30 18:34:44 +0900

    기능 : 데이터입력

--- main.py
+++ main.py
@@ -20,27 +20,24 @@ import html5lib
 # : 15개 경기 선정 기준은     
 # 1. 경기시간 > 2. 대회 우선순위(TBD) > 3. 좌측팀 가나다순
 
-def DBInsert(order, time, name, result):
+def DBInsert(conn, curs, order, time, name, result):
     print(order, time, name, result)
     print()
     # sql = 'INSERT INTO SPORTS (G_ORDER, C_TIME, G_TIME, G_NAME, G_RESULT) VALUES(%s, now(), %s, %s, %s) ON DUPLICATE KEY UPDATE G_ORDER = %s, G_TIME = %s, G_NAME = %s, G_RESULT = %s'
     # data = (order, time, name, result, order, time, name, result)
-    # curs.execute(sql, data)
-    # conn.commit()
-    # print("** 데이터 입력 **")
+    sql = 'INSERT INTO SPORTS (G_ORDER, C_TIME, G_TIME, G_NAME, G_RESULT) VALUES(%s, now(), %s, %s, %s)'
+    data = (order, time, name, result)
+    curs.execute(sql, data)
+    conn.commit()
+    print("** 데이터 입력 **")
 
 def findGame(soup, needed, gameDate, conn, curs, count):
-    #print(gameDate)
-
     scoreboard = soup.find("table", class_="tbl_scoreboard_day")
 
     for gameName in soup.find_all("td", class_="game"):
         for i in range(0, len(needed)):
             if needed[i] == gameName.get_text():
-                #print("@@@ " + needed[i])
                 # 본인 tr + 본 게임에 해당되는 다른 tr들 가져오기
-                #print(gameName.get_text())
-                #print(gameName.find_previous("tr"))
                 thisClass = gameName.find_previous("tr")
                 tr = thisClass.find_all("tr")[1] 
                 if not 'ing' in tr["class"]:
@@ -48,7 +45,7 @@ def findGame(soup, needed, gameDate, conn, curs, count):
                         score0 = getScore(state)
                         if score0 is not None:
                             count +=1
-                            DBInsert(count, gameDate, needed[i], score0)
+                            DBInsert(conn, curs, count, gameDate, needed[i], score0)
                 
                         for o in tr.find_all_next("tr"):
                             if o.get("class") == None:
@@ -61,7 +58,7 @@ def findGame(soup, needed, gameDate, conn, curs, count):
                                         score1 = getScore(state1)
                                         if score1 is not None:
                                             count +=1
-                                            DBInsert(count, gameDate, needed[i], score1)
+                                            DBInsert(conn, curs, count, gameDate, needed[i], score1)
                 print("=====================================================")
     
     return count
@@ -91,11 +88,8 @@ def main(logger):
         count = 0
 
         # DB
-        # conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
-        # curs = conn.cursor()
-        conn = ""
-        curs = ""
-
+        conn = pymysql.connect(host=TargetConfig.DB_HOST, user=TargetConfig.DB_USER, password=TargetConfig.DB_PW, db=TargetConfig.DB_NAME, charset='utf8')
+        curs = conn.cursor()
         print(" == start == ")
 
         frame = TargetConfig.SPORTS
